d93561533547d48097298cb4a0479f1e16a3e22d
matiasb7 <matias.berger@rockstarcoders.com>
2024-01-20 00:57:20 -0300

    Update package.json

--- src/Autocomplete.jsx
+++ src/Autocomplete.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useCloseDropdown } from './hooks/useCloseDropdown.js';
 import './styles/styles.css';
 import useAccessibleDropdown from './hooks/useAccessibleDropdown.js';
